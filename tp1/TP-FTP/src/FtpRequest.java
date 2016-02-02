a7feed85dfd63f6fbd28235f1b85ce43d5121539
BOUKROU <malik.boukrou@gmail.com>
2016-02-02 17:26:28 +0100

    rien

--- tp1/TP-FTP/src/FtpRequest.java
+++ tp1/TP-FTP/src/FtpRequest.java
@@ -1,5 +1,32 @@
 
 public class FtpRequest {
 
-	
+
+	public void processRequest(){
+
+	}
+
+	public void processUSER(){
+
+	}
+
+	public void processPASS(){
+
+	}
+
+	public void processRETR(){
+
+	}
+
+	public void processSTOR(){
+
+	}
+
+	public void processLIST(){
+
+	}
+
+	public void processQUIT(){
+
+	}
 }
