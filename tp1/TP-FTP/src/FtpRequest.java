971947ed9a328afb86f04f3fbcad57fb8375ede2
jf <jfdurk@gmx.fr>
2016-02-09 17:24:15 +0100

    requete avancée

--- tp1/TP-FTP/src/FtpRequest.java
+++ tp1/TP-FTP/src/FtpRequest.java
@@ -10,12 +10,25 @@ import java.io.BufferedWriter;
 public class FtpRequest {
 
 	private Socket s;
+	private	String adr;
+	private int port;
+	private boolean auth;
 	private BufferedReader r;
 	private OutputStreamWriter w;
+	private String user;
+	private String pass;
+	private String path;
 
+	private Socket ds;
+	private BufferedReader dr;
+	private OutputStreamWriter dw;
+	private byte[] data; //pas sur
+	
 	public FtpRequest(Socket s){
 		this.s = s;
-
+		this.adr = this.s.getLocalAddress().getHostAddress();
+		this.port = 3000;
+		this.auth = false;
 		try{
 			r = new BufferedReader(new inputStreamReader(this.s.getInputStream()));
 			w = new OutputStreamWriter(this.s.getOutputStream());
@@ -23,9 +36,21 @@ public class FtpRequest {
 		catch (IOexception e){
 			System.out.println(e);
 		}
+		this.user = "user";
+		this.pass = "pass";
+		this.path = System.getProperty(user.home);
+
+		//définir le data
+		this.data;
+		//définir un datasocket
+		this.ds;
+		this.dr;
+		this.dw;
 	}
 
-	public void processRequest(String req){
+	public void processRequest(){
+		String req;
+
 		if (req.contains("USER"))
 			processUSER(req);
 		if (req.contains("PASS"))
@@ -40,22 +65,48 @@ public class FtpRequest {
 			processQUIT(req);
 	}
 
-	private String processUSER(String req){
-		System.out.println ("USER : "+req);
-
-
-
-		return "TRUE";
+	private void processUSER(String req){
+		if (!this.auth && this.user.equals(this.data)){
+			this.auth = true;
+			System.out.println("331 : user ok");
+		}
+		else
+			System.out.println("530 : bad user");
 	}
 
-	private String processPASS(String req){
-		
-		return "TRUE";
+	private void processPASS(String req){
+		if (this.auth && this.pass.equals(this.data))
+			System.out.println("230 : login ok");
+		else
+			System.out.println("530 : bad password");
 	}
 
-	private String processRETR(String req){
-		
-		return "TRUE";
+	private void processRETR(String req){
+		Path file = Paths.get(this.path);
+
+        try {
+            if (this.auth) {
+                byte[] dataArray = Files.readAllBytes(fileToTransfer.resolve(this.data));
+
+                this.ds = new Socket(InetAddress.getByName(this.adr), this.port);
+
+                this.dr = new DataInputStream(this.ds.getInputStream());
+                this.dw = new DataOutputStream(this.ds.getOutputStream());
+
+                this.out.println("150 : File OK");
+
+                for (int i = 0; i < dataArray.length; i++) {
+                    this.dw.writeByte(dataArray[i]);
+                    this.dw.flush();
+                }
+                this.ds.close();
+                System.out.println("226 : data closed");
+            }
+            else
+                System.out.println("530 : Bad connexion");
+
+            } 
+        catch (IOException e) {System.out.println("550 : error connexion or login");}
 	}
 
 	private String processSTOR(String req){
