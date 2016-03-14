beef7c3cf3d71a47b1480498703929823e965464
jf <jfdurk@gmx.fr>
2016-03-14 17:53:30 +0100

    read

--- tp2/src/tp2/FTPResource.java
+++ tp2/src/tp2/FTPResource.java
@@ -45,6 +45,8 @@ public class FTPResource {
                 "</p></body></html>";
     }
 
+    /* -- LOGIN --*/
+
 	/**
 	 * Login
 	 * @return un formulaire de connexion
@@ -72,6 +74,141 @@ public class FTPResource {
 				"</body>" +
 				"</html>";
 	}
+
+	/**
+     * Login test sur le serveur FTP
+     * @return un message d'erreur ou la liste des fichiers
+     */
+    @POST
+    @Path("/data")
+    public String LogInAction(@FormParam( "name" )final String name,
+                              @FormParam( "pass" ) final String password )throws IOException  {
+
+        commandes.CMDUSER(name);
+        if(commandes.CMDPASS(password)){
+            this.login = true;
+            return getFiles();
+        }
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
+                "<html xmlns=\"<xmlns />\" xml:lang=\"en\">"+
+                "<head>" +
+                "<title>Login</title>" +
+                " </head>" +
+                "<body>" +
+                " <form action=\"/rest/api/res/login\" method=\"GET\">" +
+                "<label for=\"name\">Login fail !</label>" +
+                " <br/>" +
+                "<input type=\"submit\" value=\"Submit\" />" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+    /**
+     * Logout
+     * @return Un formulaire de connexion
+     */
+    @GET
+    @Path("/logout")
+    @Produces("text/html")
+    public String logOut() throws IOException {
+
+        this.login = false;
+        return logIn();
+
+    }
+
+    /* -- READ -- */
+    /**
+     * Lit le fichier "name"
+     * @return  Le contenu du fichier "name" ou la liste des fichiers si "name" est un dossier.
+     */
+    @GET
+    @Path("/data/{name}")
+    @Produces("text/html")
+    public String getFile(@PathParam( "name" ) String name)throws IOException  {
+
+        if(!login)
+            return logIn();
+
+        if(this.commandes.CMDCWD("/data")){
+            String result = searchFile(name);
+            if(this.commandes.getCurrentDir().equals("data")){
+                result += "<form action=\"/rest/api/res/"+this.commandes.getCurrentDir()+"/"+name+"/edit\">"+
+                        "<input type=\"submit\" value=\"Edit\">"+
+                        "</form>";
+                result += "<button onclick= \"p()\">Delete</button>"+
+                        "<script type=\"text/javascript\">"+
+                        "function p(){"+
+                        "console.log(\"Ici\");"+
+                        "xhr=window.ActiveXObject ? new ActiveXObject(\"Microsoft.XMLHTTP\") : new XMLHttpRequest();"+
+                        "xhr.onreadystatechange=function(){};"+
+                        "xhr.open(\"DELETE\", \"http://localhost:8080/rest/api/res/data/"+name+"\");"+
+                        "xhr.send(null);"+
+                        "};"+
+                        "</script>";
+                result +="<form action=\"/rest/api/res/data\">"+
+                        "<input type=\"submit\" value=\"Return\">"+
+                        "</form></br>";
+            }else{
+                result +="<form action=\"/rest/api/res/"+this.commandes.getCurrentDir()+"/add\">"+
+                        "<input type=\"submit\" value=\"Add File\">"+
+                        "</form></br>";
+                result +="<form action=\"/rest/api/res/logout\">"+
+                        "<input type=\"submit\" value=\"Logout\">"+
+                        "</form></br>";
+            }
+            return result;
+        }
+        return "<h1>PATH NOT FOUND</h1>";
+    }
+
+    /**
+     * Lit le fichier "dir/name"
+     * @return  Le contenu du fichier "dir/name" ou la liste des fichiers si "dir/name" est un dossier.
+     */
+    @GET
+    @Path("/data/{dir}/{name}")
+    @Produces("text/html")
+    public String getFile(@PathParam( "name" ) String name,@PathParam( "dir" ) String dir)throws IOException  {
+
+        if(!login)
+            return logIn();
+
+        System.out.println(this.commandes.getCurrentDir());
+        if(this.commandes.CMDCWD("/data/"+dir)){
+            String result = searchFile(name);
+            if(this.commandes.getCurrentDir().equals("data/"+dir)){
+                result += "<form action=\"/rest/api/res/"+this.commandes.getCurrentDir()+"/"+name+"/edit\">"+
+                        "<input type=\"submit\" value=\"Edit\">"+
+                        "</form></br>";
+                result += "<button onclick= \"p()\">Delete</button>"+
+                        "<script type=\"text/javascript\">"+
+                        "function p(){"+
+                        "console.log(\"Ici\");"+
+                        "xhr=window.ActiveXObject ? new ActiveXObject(\"Microsoft.XMLHTTP\") : new XMLHttpRequest();"+
+                        "xhr.onreadystatechange=function(){};"+
+                        "xhr.open(\"DELETE\", \"http://localhost:8080/rest/api/res/data/"+dir+"/"+name+"\");"+
+                        "xhr.send(null);"+
+                        "};"+
+                        "</script>";
+                result +="<form action=\"/rest/api/res/data/"+dir+"\">"+
+                        "<input type=\"submit\" value=\"Return\">"+
+                        "</form></br>";
+            }else{
+                result +="<form action=\"/rest/api/res/"+this.commandes.getCurrentDir()+"/add\">"+
+                        "<input type=\"submit\" value=\"Add File\">"+
+                        "</form></br>";
+                result +="<form action=\"/rest/api/res/logout\">"+
+                        "<input type=\"submit\" value=\"Logout\">"+
+                        "</form></br>";
+            }
+            return result;
+
+        }
+        return "<h1>PATH NOT FOUND</h1>";
+    }
+    
 
 }
 
