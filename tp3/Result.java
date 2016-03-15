992e66485027d201479fd68c0d9caffed1c29322
malikboukrou <malik.boukrou@gmail.com>
2016-03-15 18:45:28 +0100

    tp tuto added to understand akka

---
+++ tp3/Result.java
@@ -0,0 +1,12 @@
+
+public class Result {
+
+	private BigInteger bigInt;
+
+	public Result(BigInteger bigInt) {
+		this.bigInt = bigInt;}
+
+	public BigInteger getFactorial() {
+		return this.bigInt;
+	}
+}
